ac2f175749f022a5f0facb2a98dfcc48302e7c94
Ct7liang <ct7liang@163.com>
2018-04-03 15:08:02 +0800

    修改标题栏高度为wrap_content

--- app/src/main/java/com/ct7liang/developer/MainActivity.java
+++ app/src/main/java/com/ct7liang/developer/MainActivity.java
@@ -1,8 +1,7 @@
 package com.ct7liang.developer;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 
@@ -13,13 +12,18 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    public void getOnCreateParams(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void setStatusBar() {
+        //用于实现沉浸式状态栏
+    }
+
+    @Override
     public void findView() {
         //查找控件
-        findViewById(R.id.left_image).setOnClickListener(this);
-        ((TextView)findViewById(R.id.center_text)).setText("首页");
-        ((TextView)findViewById(R.id.right_text)).setText("更多");
-//        ((TextView)findViewById(R.id.left_text)).setText("关于");
-        findViewById(R.id.right).setOnClickListener(this);
     }
 
     @Override
@@ -40,15 +44,6 @@ public class MainActivity extends BaseActivity {
     @Override
     public void onClick(View v) {
         //点击事件处理
-        switch (v.getId()){
-            case R.id.left_image:
-                exitApp();
-                break;
-            case R.id.right:
-//                ToastUtils.showStatic(mAct, "Hello World!");
-                startActivity(new Intent(this, StatusBarActivity.class));
-                break;
-        }
     }
 
     @Override
